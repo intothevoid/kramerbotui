1717ed0b56d12a0ec06f7f884432fb0bb6136475
Karan Kadam <karank@outlook.com>
2023-05-28 21:44:06 +0930

    refactor: 💡 improved css

--- src/Login.svelte
+++ src/Login.svelte
@@ -9,14 +9,14 @@
 </script>
 
 <div class="flex flex-col items-center justify-center min-h-screen bg-gray-200">
-    <h1 class="py-10 mb-10 text-5xl">Kramer's deals.</h1>
-    <h1 class="py-4 mb-4 text-xl text-gray-600">
+    <h1 class="py-10 mb-10 text-5xl">Kramer's Deals.</h1>
+    <h1 class="py-4 mb-4 text-md text-gray-600">
         Please login or register to continue.
     </h1>
     <div class="p-4 bg-white rounded shadow">
         <div>
             <input
-                class="mb-2 py-2 input input-bordered"
+                class="mb-2 px-3 py-2 input input-bordered border border-slate-200 rounded-md shadow-sm"
                 type="text"
                 bind:value={username}
                 placeholder="Username"
@@ -24,19 +24,19 @@
         </div>
         <div>
             <input
-                class="mb-2 py-2 input input-bordered"
+                class="mb-2 px-3 py-2 input input-bordered border border-slate-200 rounded-md shadow-sm"
                 type="password"
                 bind:value={password}
                 placeholder="Password"
             />
         </div>
-        <div class="mt-10">
+        <div class="mt-5">
             <button
-                class="btn btn-primary bg-blue-400 px-8 py-2 text-white rounded-lg hover:bg-blue-600"
+                class="btn btn-primary mb-2 bg-blue-400 px-2 py-2 text-white rounded-lg hover:bg-blue-600 w-full"
                 on:click={login}>Login</button
             >
             <button
-                class="btn btn-primary bg-green-400 ml-2 px-5 py-2 text-white rounded-lg hover:bg-green-600"
+                class="btn btn-primary bg-green-400 px-2 py-2 text-white rounded-lg hover:bg-green-600 w-full"
                 on:click={login}>Register</button
             >
         </div>
