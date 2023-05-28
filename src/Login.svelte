b8247b0ccc52e90955623f356092e9855ced8ee3
Karan Kadam <karank@outlook.com>
2023-05-28 21:24:55 +0930

    feat: 🎸 added login screen

---
+++ src/Login.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { onMount } from "svelte";
+    let username = "";
+    let password = "";
+
+    async function login() {
+        // login API call
+    }
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen bg-gray-200">
+    <h1 class="py-10 mb-10 text-5xl">Kramer's deals.</h1>
+    <h1 class="py-4 mb-4 text-xl text-gray-600">
+        Please login or register to continue.
+    </h1>
+    <div class="p-4 bg-white rounded shadow">
+        <div>
+            <input
+                class="mb-2 py-2 input input-bordered"
+                type="text"
+                bind:value={username}
+                placeholder="Username"
+            />
+        </div>
+        <div>
+            <input
+                class="mb-2 py-2 input input-bordered"
+                type="password"
+                bind:value={password}
+                placeholder="Password"
+            />
+        </div>
+        <div class="mt-10">
+            <button
+                class="btn btn-primary bg-blue-400 px-8 py-2 text-white rounded-lg hover:bg-blue-600"
+                on:click={login}>Login</button
+            >
+            <button
+                class="btn btn-primary bg-green-400 ml-2 px-5 py-2 text-white rounded-lg hover:bg-green-600"
+                on:click={login}>Register</button
+            >
+        </div>
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: false,
   content: [],
+  purge: ['./index.html', './src/**/*.{svelte,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
