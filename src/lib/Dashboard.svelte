8812fd34a5e54147b96c41449ef4ff217bef665f
Karan Kadam <karank@outlook.com>
2023-05-31 23:20:09 +0930

    feat: 🎸 made the dashboard pretty :)

--- src/lib/Dashboard.svelte
+++ src/lib/Dashboard.svelte
@@ -2,12 +2,12 @@
     import { userStore } from "./userStore";
 
     let username = ""; // fetch username from API
-    let keyword = "";
-    let keywords = [];
+    let watch = "";
+    let watches = [];
 
-    function addKeyword() {
-        keywords = [...keywords, keyword];
-        keyword = "";
+    function addWatch() {
+        watches = [...watches, watch];
+        watch = "";
     }
 
     function logout() {
@@ -19,34 +19,39 @@
     }
 </script>
 
-<div class="container mx-auto px-4">
+<div class="mx-auto px-4 bg-gray-100 rounded shadow-lg p-6 min-h-screen">
     <div class="flex items-center justify-between py-4">
-        <h1 class="text-lg">Hello, {username}</h1>
-        <button class="btn btn-primary" on:click={logout}>Logout</button>
+        <h1 class="text-lg">Hello {username !== "" ? username : "User"}!</h1>
+        <button
+            class="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded"
+            on:click={logout}>Logout</button
+        >
     </div>
 
-    <div class="mb-4">
-        <input type="checkbox" class="checkbox checkbox-primary" /> Checkbox 1
-        <input type="checkbox" class="checkbox checkbox-primary" /> Checkbox 2
-        <input type="checkbox" class="checkbox checkbox-primary" /> Checkbox 3
-        <input type="checkbox" class="checkbox checkbox-primary" /> Checkbox 4
-    </div>
-
-    <div class="mb-4">
+    <div class="flex justify-center">
         <input
-            class="input input-bordered"
+            class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
             type="text"
-            bind:value={keyword}
-            placeholder="Add keyword"
+            bind:value={watch}
+            placeholder="Enter keyword(s) to watch"
         />
-        <button class="btn btn-primary" on:click={addKeyword}>Add</button>
+        <button
+            class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+            on:click={addWatch}>Add Watch</button
+        >
     </div>
 
-    <div>
-        <ul>
-            {#each keywords as keyword}
-                <li>{keyword}</li>
-            {/each}
-        </ul>
-    </div>
+    {#if watches.length > 0}
+        <div class="mt-4">
+            <h2 class="text-lg mb-2">Your watched words / phrases:</h2>
+            <div class="flex flex-wrap gap-2">
+                {#each watches as watch}
+                    <span
+                        class="bg-gray-400 rounded-full text-xs font-semibold text-white py-1 px-4"
+                        >{watch}</span
+                    >
+                {/each}
+            </div>
+        </div>
+    {/if}
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: false,
   content: [],
   purge: ['./index.html', './src/**/*.{svelte,js,ts,jsx,tsx}'],
   theme: {
